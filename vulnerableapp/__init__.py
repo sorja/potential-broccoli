6b47962ca0040325bf4012e999f68bd0a1d676a1
sorja <miro.sorja@gmail.com>
2017-01-29 23:00:14 +0200

    update

--- vulnerableapp/__init__.py
+++ vulnerableapp/__init__.py
@@ -17,7 +17,26 @@ if __name__ == "__main__":
 
 @app.route("/")
 def landing():
+    if 'user' in session.keys():
+        user_messages(session['user'][0])
     return render_template('landing.html')
+
+@app.route("/messages", methods=['POST', 'GET'])
+def messages():
+    print request.form
+    print request.method
+    if request.method == 'POST':
+        parent_id = request.form['parent_id'] or None
+        message = request.form['message']
+        # this is vulnerability
+        email = request.form['email']
+        user_id = user_messages(email)[0]['user_id']
+        message_create(user_id, message, parent_id)
+    if request.method == 'GET':
+        user_messages(session['email'])
+    return render_template('landing.html')
+
+    
 
 @app.route('/register', methods=['POST'])
 def register():
@@ -35,8 +54,9 @@ def login():
         email = request.form['email']
         password = request.form['password']
         if email and password:
-            user_login(email, password)
+            session['user'] = user_login(email, password)
             session['email'] = request.form['email']
+            session['user_messages'] = user_messages(session['user']['id'])
             redirect(url_for('landing'))
     return redirect(url_for('landing'))
 
@@ -44,6 +64,7 @@ def login():
 def logout():
     if session['email']:
         session.pop('email', None)
+        session.pop('user', None)
     return redirect(url_for('landing'))
 
 
@@ -62,11 +83,46 @@ def user_login(email, password):
     finally:
         cursor.close()
         conn.close()
-
+    return result
 
 def user_register(email, password):
     SQL = "INSERT INTO users (email, password) VALUES (%s, %s) RETURNING *"
     data = (email, password)
+    conn = psycopg2.connect("dbname=broccoli user=possibly")
+    cursor = conn.cursor(cursor_factory=DictCursor)
+    try:
+        cursor.execute(SQL, data)
+        result = cursor.fetchone()
+    except:
+        conn.rollback()
+        raise
+    finally:
+        cursor.close()
+        conn.close()
+    return result
+
+def user_messages(user_id):
+    SQL = " SELECT * FROM messages WHERE user_id=%s"
+    data = [user_id]
+    conn = psycopg2.connect("dbname=broccoli user=possibly")
+    cursor = conn.cursor(cursor_factory=DictCursor)
+    try:
+        cursor.execute(SQL, data)
+        result = cursor.fetchmany()
+    except:
+        conn.rollback()
+        raise
+    finally:
+        cursor.close()
+        conn.close()
+    return result
+
+def message_create(user_id, message, parent_id = None):
+    SQL = "INSERT INTO users (user_id, message) VALUES (%s, %s) RETURNING *"
+    data = (user_id, message, parent_id)
+    
+    if parent_id:
+        SQL = "INSERT INTO users (user_id, message, parent_id) VALUES (%s, %s, %s) RETURNING *"
     conn = psycopg2.connect("dbname=broccoli user=possibly")
     cursor = conn.cursor(cursor_factory=DictCursor)
     try:
